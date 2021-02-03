d47d66a0d5faf661e63f6584be621b297c3d0982
UnaiInfante <uinfantega19dw31@ikzubirimanteo.com>
2021-02-03 18:58:20 +0100

    project working fine

--- resources/views/ourProfile/profile.blade.php
+++ resources/views/ourProfile/profile.blade.php
@@ -6,7 +6,7 @@
         <section class="col-12 col-md-3">
             <div class="row ">
                 <div class="col-12 d-flex justify-content-center">
-                    <img style="width: 200px; height: 200px" class="rounded-circle" src="../../../public/storage/{{$user->img}}" alt="{{$user->img}}" >
+                    <img style="width: 200px; height: 200px" class="rounded-circle" src="{{asset('/storage/'.$user->img)}}" alt="{{$user->img}}" >
                 </div>
             </div>
         </section>
